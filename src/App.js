5b00caee457d3896f8a585b4bc3dc40a1bc7c950
Sarthak Gupta <sarthakgupta878@gmail.com>
2021-11-22 17:35:10 +0530

    added dark mode option

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 
+import React, { useState } from 'react'
+
 import './App.css';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
+
+
 function App() {
+  const [mode,setMode] = useState('light');
+
+const toggleMode=()=>{
+  if(mode === 'light' ){
+    setMode('dark');
+    document.body.style.backgroundColor='#042743';
+  }
+  else{
+    setMode('light');
+    document.body.style.backgroundColor='white';
+
+  }
+}
+
   return (
    <>
-<Navbar title ="TextUtils" aboutText="About" />
+<Navbar title ="TextUtils" aboutText="About" mode ={mode} toggleMode={toggleMode} />
 <div className="container my-3">
-<TextForm heading="Enter the Text to analyse below" />
+<TextForm heading="Enter the Text to analyse below" mode ={mode}/>
 {/* <About/> */}
 </div>
    </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,50 +1,59 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 
 
 export default function TextForm(props) {
-    const handleUpClick=()=>{
-        let newText=text.toUpperCase();
+    const handleUpClick = () => {
+        let newText = text.toUpperCase();
         setText(newText);
 
     }
-    const handleLoClick=()=>{
-        let newText=text.toLowerCase();
+    const handleLoClick = () => {
+        let newText = text.toLowerCase();
         setText(newText);
 
     }
-    const ClearOnClick=()=>{
-        let newText='';
+    const ClearOnClick = () => {
+        let newText = '';
         setText(newText);
 
     }
-    const handleOnChange=(event)=>{
+    const handleOnChange = (event) => {
         // console.log('change')
         setText(event.target.value);
     }
-    
+    const handleCopy = () => {
+        var text = document.getElementById('myBox');
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+    }
 
-    const [text,setText]=useState('Enter text here');
+
+    const [text, setText] = useState('');
     return (
         <>
-        <div className="container">
-            <h1>{props.heading}</h1>
-            <div className="mb-3">
-            <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+            <div className="container" style={{ color: props.mode === 'dark' ? 'white' : '#042743' }}>
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea className="form-control" value={text} onChange={handleOnChange} style={{ backgroundColor: props.mode === 'dark' ? 'grey' : 'white', color: props.mode === 'dark' ? 'white' : '#042743' }} id="myBox" rows="8"></textarea>
+                </div>
+                <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+                <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
+                <button className="btn btn-primary mx-2" onClick={ClearOnClick}>Clear Text</button>
+                <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy Text</button>
+                <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
             </div>
-            <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
-            <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
-            <button className="btn btn-primary mx-2" onClick={ClearOnClick}>Clear Text</button>
+            <div className="container my-2" style={{ color: props.mode === 'dark' ? 'white' : '#042743' }}>
+                <h2>Your Text summery</h2>
+                <p>{text.split(" ").length}words and {text.length} characters</p>
+                <p> {0.008 * text.split(" ").length}Minutes read </p>
+                <h2>Preview</h2>
+                <p> {text.length>0?text:"Enter something in the textbox above to preview it here"} </p>
 
-
-        </div>
-        <div className="container my-2">
-            <h2>Your Text summery</h2>
-            <p>{text.split(" ").length}words and {text.length} characters</p>
-            <p> {0.008* text.split(" ").length}Minutes read </p>
-            <h2>Preview</h2>
-            <p> {text} </p>
-
-        </div>
+            </div>
         </>
     )
 }
