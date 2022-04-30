edcbf96b2070ad943d1893d34a0db55e0f533a68
Sarthak Gupta <sarthakgupta878@gmail.com>
2022-04-30 13:14:34 +0530

    added a color pallet

--- src/App.js
+++ src/App.js
@@ -31,8 +31,24 @@ function App() {
     }, 2000);
   }
 
-  const toggleMode = () => {
+  const removeBodyClasses=()=>{
+    document.body.classList.remove('bg-light')
+    document.body.classList.remove('bg-dark')
+    document.body.classList.remove('bg-warning')
+    document.body.classList.remove('bg-danger')
+    document.body.classList.remove('bg-success')
+    document.body.classList.remove('bg-primary')
+
+
+
+  }
+
+  const toggleMode = (cls) => {
+    removeBodyClasses();
+
+    document.body.classList.add('bg-'+cls)
     if (mode === 'light') {
+
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
       showAlert("Dark mode has been enabled", "success");
