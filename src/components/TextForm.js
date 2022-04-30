92d68973ec90e6e763a7f5dd3e7d7cc1d66a34ca
Sarthak Gupta <sarthakgupta878@gmail.com>
2022-04-30 13:29:02 +0530

    Project compleated plz remove toggol button/colour Pallat

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -29,10 +29,10 @@ export default function TextForm(props) {
         
     }
     const handleCopy = () => {
-        var text = document.getElementById('myBox');
-        text.select();
-        navigator.clipboard.writeText(text.value);
-        document.getSelection().removeAllRanges();
+        // var text = document.getElementById('myBox');
+        // text.select();
+        navigator.clipboard.writeText(text);
+        // document.getSelection().removeAllRanges();
         props.showAlert("Text Copied to Clipboard","success");
 
     }
@@ -60,7 +60,7 @@ export default function TextForm(props) {
             </div>
             <div className="container my-2" style={{ color: props.mode === 'dark' ? 'white' : '#042743' }}>
                 <h2>Your Text summery</h2>
-                <p>{text.split(" ").filter((element)=>{return element.length!==0}).length}words and {text.length} characters</p>
+                <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length}words and {text.length} characters</p>
                 <p> {0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes read </p>
                 <h2>Preview</h2>
                 <p> {text.length>0?text:"Nothing to preview it here!!"} </p>
